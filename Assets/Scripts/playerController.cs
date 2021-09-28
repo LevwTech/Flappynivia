4191d4abc5b8d1875445d90a871ffdc307887a89
levvw <abdelraahmanmostafa@gmail.com>
2021-09-28 14:00:08 +0200

    added game files

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class playerController : MonoBehaviour
+{
+    [SerializeField] TMPro.TextMeshProUGUI scoretext;
+
+    Rigidbody2D bird;
+
+    int score = 0;
+
+    bool dead = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bird = transform.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            bird.velocity = new Vector2(0, 8.5f);
+        }
+        if(Input.GetKeyDown("r"))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    void OnCollisionEnter2D()
+    {
+        dead = true;
+   
+
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+
+    {
+        if(col.gameObject.tag == "pointtrigger")
+        {
+            score++;
+            scoretext.text = score.ToString();
+        }
+        if (col.gameObject.tag == "restart")
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/wallgenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using UnityEngine;
+
+
+public class wallgenerator : MonoBehaviour
+{
+    [SerializeField] GameObject walls;
+    int timer = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        timer++;
+        if (timer >= 100)
+        {
+            timer = 0;
+            GameObject wall = Instantiate(walls, new Vector2(walls.transform.position.x, walls.transform.position.y + Random.Range(-3, 3)), walls.transform.rotation);
+            Destroy(wall, 5);
+          
+        }
+    }
+}
